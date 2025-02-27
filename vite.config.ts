85f7cd9644e7b1a1d7c93f7c20cce95be3a1526d
Daniil Kim <73555295+DaniilKimlb@users.noreply.github.com>
2025-02-27 15:44:27 +0100

    fix: added prefix in base

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  base: '/',
+  base: '/p2p',
   plugins: [
     VueMacros({
       defineOptions: false,
