e72e45940638cd4355dc6d1d02fa2257678e9528
daniil <dan.kim.04lb@gmail.com>
2025-02-28 23:23:02 +0100

    feat: remove api url env variable  to hardcode

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="max-w-7xl w-full mx-auto px-4">
-    <header class="py-6 text-center text-3xl font-bold text-[#ff0078]">
+    <header class="py-6 text-center text-2xl font-bold text-[#ff0078]">
       PKOIN Exchange
     </header>
     <RouterView />
