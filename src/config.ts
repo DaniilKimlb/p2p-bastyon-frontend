4c1c66ba99a542acd836461c967fc3695c101762
Daniil Kim <73555295+DaniilKimlb@users.noreply.github.com>
2025-02-28 16:53:24 +0100

    feat: new makers in config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const APP_API_URL = import.meta.env.VITE_APP_API_URL
-export const makers = ['PMZ3DiGWKGybLb5oCz9ojwxuTBA6GcYAKq', 'PPVgnH4N22yriNu9HsvViXjshNFy3BLqoJ', 'PLpzAiA6H8isp33WeVx2UEuXLfc3SyqkzK', 'PEL45oK7ppFhB69G6GSsMLxHatNu5FJc5P']
+export const makers = ["PTDms3ANccr7tJFru6ifnLgJKjHUSa3BsE", 'PMZ3DiGWKGybLb5oCz9ojwxuTBA6GcYAKq', 'PPVgnH4N22yriNu9HsvViXjshNFy3BLqoJ', 'PLpzAiA6H8isp33WeVx2UEuXLfc3SyqkzK', 'PEL45oK7ppFhB69G6GSsMLxHatNu5FJc5P']
